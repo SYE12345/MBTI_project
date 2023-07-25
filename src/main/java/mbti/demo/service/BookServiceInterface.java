0ed076875a24355f111997c71aee86cab6e1d4ab
SYE12345 <129352928+SYE12345@users.noreply.github.com>
2023-07-25 15:10:50 +0900

    성예은

--- src/main/java/mbti/demo/service/BookServiceInterface.java
+++ src/main/java/mbti/demo/service/BookServiceInterface.java
@@ -1,12 +1,13 @@
 package mbti.demo.service;
 import mbti.demo.domain.Book;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 public interface BookServiceInterface {
     Book save(Book book);
     List<Book> findAll();
-    List<Book> findByAnalystType(); //분석가형
-    List<Book> findByDiplomaticType(); //외교관형
-    List<Book> findByAdministratorType(); //관리자형
-    List<Book> findByExplorerType(); //탐험가형
+    Page<Book> findByAnalystType(Pageable pageable); //분석가형
+    Page<Book> findByDiplomaticType(Pageable pageable); //외교관형
+    Page<Book> findByAdministratorType(Pageable pageable); //관리자형
+    Page<Book> findByExplorerType(Pageable pageable); //탐험가형
 }
