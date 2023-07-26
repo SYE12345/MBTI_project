8a4c391175f49a221dee0530932efc0a98941a71
junmay <bsy00133@naver.com>
2023-07-26 15:21:29 +0900

    박준혁

--- Flask/mbti.py
+++ Flask/mbti.py
@@ -3,14 +3,6 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from bs4 import BeautifulSoup
 from flask_cors import CORS
-<<<<<<< HEAD
-from webdriver_manager.chrome import ChromeDriverManager
-import time
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-import re
-from flask import Flask, render_template, request, jsonify
-=======
 import time
 from selenium.webdriver.chrome.options import Options
 import re
@@ -18,7 +10,6 @@ from flask import Flask, render_template, request, jsonify
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
->>>>>>> jun/master
 
 app = Flask(__name__, template_folder='templates')
 CORS(app)
@@ -31,13 +22,8 @@ def run_mbti_test():
     print('Debug2')
     driver.get("https://www.16personalities.com/ko/%EB%AC%B4%EB%A3%8C-%EC%84%B1%EA%B2%A9-%EC%9C%A0%ED%98%95-%EA%B2%80%EC%82%AC")
     print('Debug3')
-<<<<<<< HEAD
-    driver.implicitly_wait(10)
-    button = driver.find_element_by_xpath('//*[@class="card__text"]')
-=======
     driver.implicitly_wait(3600)
     button = driver.find_element(By.XPATH, '//*[@class="card__text"]')
->>>>>>> jun/master
     ActionChains(driver).click(button).perform()
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     table = soup.find('div', {'class': ['results__type__code results__type__code--blue',
@@ -56,11 +42,7 @@ def run_mbti_test():
 
 @app.route('/')
 def index():
-<<<<<<< HEAD
-    return render_template('member/join.html')
-=======
     return render_template('join.html')
->>>>>>> jun/master
 
 @app.route('/start_mbti_test', methods=['GET'])
 def start_mbti_test():
