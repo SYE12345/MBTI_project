34a7ca5ef878e954b54f35f8e7e6427d7d37ad47
K1MCY0 <codus5728@gmail.com>
2023-08-23 18:08:42 +0900

    김채

--- src/main/java/mbti/demo/web/CommunityController.java
+++ src/main/java/mbti/demo/web/CommunityController.java
@@ -1,15 +1,14 @@
 package mbti.demo.web;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mbti.demo.domain.Community;
 import mbti.demo.repository.CommunityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,38 +37,32 @@ public class CommunityController {
     }
 
 
-//    @GetMapping("/{loginId}/community_login")
-//    public String All_list_login(@PathVariable String loginId, Model model){
-//        List<Community> list = communityRepository.findAll();
-//        List<Comment> comments = commentRepository.findAll();
-//        Member member = memberServiceInterface.findByLoginId(loginId).get();
-//        model.addAttribute("member",member);
-//        model.addAttribute("list", list);
-//        model.addAttribute("comments",comments);
-//        return "index_login";
-//    }
-//
-//    @PostMapping("/{loginId}/community_login")
-//    public String All_list(@PathVariable String loginId, Community community, Model model){
-//
-//        communityRepository.save(community);
-//        List<Community> list = communityRepository.findAll();
-//        List<Comment> comments = commentRepository.findAll();
-//        Member member = memberServiceInterface.findByLoginId(loginId).get();
-//        model.addAttribute("member",member);
-//        model.addAttribute("list", list);
-//        model.addAttribute("comments",comments);
-//        return "index_login";
-//    }
+    @GetMapping("/community/update/{id}")
+    public String edit(@PathVariable("id")Long id, Model model) {
+        communityRepository.getReferenceById(id);
+        Community list = new Community();
+        list.setContent(list.getContent());
+        model.addAttribute("list", list);
+        return "community/index_update";
+    }
+    @PutMapping("/community/update/{id}")
+    public String update(@Valid Community list, BindingResult result){
+        if(result.hasErrors()){
+            return "community/index_update";
+        }
+        communityRepository.save(list);
+        return "redirect: /";
+    }
 
-@GetMapping("/community/delete")
-public String test(Long id, Model model) {
-    System.out.println(id);
-    communityRepository.deleteById(id);
-//    replyRepository.save(reply);
-    List<Community> list = communityRepository.findAll();
-    model.addAttribute("list", list);
-    return "community/index_login";
-}
+
+    @GetMapping("/community/delete")
+    public String test(Long id, Model model) {
+        System.out.println(id);
+        communityRepository.deleteById(id);
+    //    replyRepository.save(reply);
+        List<Community> list = communityRepository.findAll();
+        model.addAttribute("list", list);
+        return "community/index_login";
+    }
 
 }
